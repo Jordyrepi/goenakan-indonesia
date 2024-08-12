7c9674a3aa6a6a0373654d323c622dc9c53da911
jordy repi <joju19grifith@gmail.com>
2024-08-12 16:39:16 +0700

    fix: env variable errors

--- src/utils/contenful/get-entries.ts
+++ src/utils/contenful/get-entries.ts
@@ -4,7 +4,7 @@ const { baseUrl, spaceId, accessToken, environtmentId } = contenfulConfig;
 
 export const getEntries = async (contentType: string) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_CONTENFUL_BASE_URL}/spaces/${process.env.NEXT_PUBLIC_CONTENFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENFUL_ENVIRONTMENT_ID}/entries?access_token=${process.env.NEXT_PUBLIC_CONTENFUL_ACCESS_TOKEN}&content_type=${contentType}`,
+    `${baseUrl}/spaces/${spaceId}/environments/${environtmentId}/entries?access_token=${accessToken}&content_type=${contentType}`,
   );
 
   return response.json();
